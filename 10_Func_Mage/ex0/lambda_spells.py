1e63316c623b593332da5ce22140ed49468007a8
belladone <belladone.gaming@gmail.com>
2026-05-04 15:55:37 +0200

    feat[FuncMage]: add ex0-ex4

---
+++ 10_Func_Mage/ex0/lambda_spells.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+"""Filename : lambda_spells.py
+
+Date: 2026-05-04
+Description: This program organize magical artifacts using anonymous functions.
+This is done using lambda expressions.
+"""
+
+
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    """This function sort magical artifacts using lambda expression."""
+    print("\nTesting artifact sorter...")
+    print("=" * 40)
+    print("Initial artifacts:")
+    for artifact in artifacts:
+        print(f"{artifact["name"]} ({artifact["power"]} power)")
+    sorted_artifacts = sorted(artifacts, key=lambda artifact:
+                              artifact["power"], reverse=True)
+    print("\nSorted artifacts:")
+    for artifact in sorted_artifacts:
+        print(f"{artifact["name"]} ({artifact["power"]} power)")
+    print("=" * 40)
+    return sorted_artifacts
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    """This function filter mages by power using lambda expression."""
+    print("\nTesting mage filter...")
+    print("=" * 40)
+    print("Initial mages:")
+    for mage in mages:
+        print(f"{mage["name"]} ({mage["power"]} power)")
+    filtered_mages = list(filter(lambda mage: mage["power"] >=
+                                 min_power, mages))
+    print("\nFiltered mages:")
+    for mage in filtered_mages:
+        print(f"{mage["name"]} ({mage["power"]} power)")
+    print("=" * 40)
+    return filtered_mages
+
+
+def spell_transformer(spells: list[str]) -> list[str]:
+    """This function transform spell names using lambda expression."""
+    print("\nTesting spell transformer...")
+    print("=" * 40)
+    print("Initial spells:")
+    for spell in spells:
+        print(spell)
+    transformed_spells = list(map(lambda spell: f"* {spell} *", spells))
+    print("\nTrasnformed spells:")
+    for spell in transformed_spells:
+        print(spell)
+    print("=" * 40)
+    return transformed_spells
+
+
+def mage_stats(mages: list[dict]) -> dict:
+    """This function calculate statistic unsing lambda expression."""
+    print("\nStatistic for mages:")
+    print("=" * 40)
+    stats: dict = {
+        "max_power": max(mages, key=lambda mage: mage["power"])["power"],
+        "min_power": min(mages, key=lambda mage: mage["power"])["power"],
+        "avg_power": sum(map(lambda mage: mage["power"], mages)) / len(mages)
+    }
+    print(f"Power max: {stats["max_power"]}")
+    print(f"Power min: {stats["min_power"]}")
+    print(f"Average power: {stats["avg_power"]:.2f}")
+    print("=" * 40)
+    return stats
+
+
+def main() -> None:
+    """Entry point of the program"""
+    artifacts: list[dict] = [
+        {"name": "Sacred Sword", "power": 12, "type": "sacred"},
+        {"name": "Monolith", "power": 25, "type": "evil"},
+        {"name": "Luminous Ring", "power": 14, "type": "fairy"}]
+    artifact_sorter(artifacts)
+    mages: list[dict] = [
+        {"name": "Dark Yannou", "power": 15, "element": "Darkness"},
+        {"name": "La Stige", "power": 10, "element": "Grass"},
+        {"name": "La Gige", "power": 12, "element": "Earth"},
+        {"name": "Dark Marine", "power": 14, "element": "Chaos"}]
+    power_filter(mages, 14)
+    spells: list[str] = ["poison", "fireball", "heal", "teleportation"]
+    spell_transformer(spells)
+    mage_stats(mages)
+
+
+if __name__ == "__main__":
+    main()
